794a34400b5028c5ff2555974f519326fb54088c
abdelrhman ashraf <mido59478@gmail.com>
2023-08-28 22:41:13 +0300

    changed

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import './assets/main.css'
 
 const app = createApp(App)
 
+import router from './router'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
-import App from './App.vue'
-import router from './router'
 
 
 app.use(AOS.init())
